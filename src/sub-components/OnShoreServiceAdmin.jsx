08f0dfc4fd53d7510b8ea8944b3afa3b7af2e026
Tejas <tejaswarambhe@gmail.com>
2022-07-13 04:58:44 +0530

    opk

--- src/sub-components/OnShoreServiceAdmin.jsx
+++ src/sub-components/OnShoreServiceAdmin.jsx
@@ -22,7 +22,7 @@ const OnShoreServiceAdmin = ({ inputs, setInputs }) => {
   console.log(formInputs);
 
   const sendData = async () => {
-    const response = await fetch("http://localhost:5000/upload", {
+    const response = await fetch("https://marine-form-backend.herokuapp.com/upload", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
