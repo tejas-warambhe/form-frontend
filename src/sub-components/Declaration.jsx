2bd95e92816de71a34d73fef400a6ce1f8ba38a4
Tejas <tejaswarambhe@gmail.com>
2022-07-15 02:17:05 +0530

    Image Url added to apis and Edit part done

--- src/sub-components/Declaration.jsx
+++ src/sub-components/Declaration.jsx
@@ -1,31 +1,51 @@
 import axios from "axios";
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const Declaration = ({ inputs, setInputs }) => {
-    const [imgFile, setImgFile] = useState('');
-    const [file, setFile] = useState(null);
+  const [imgFile, setImgFile] = useState({
+    formal_photo: "",
+    signature_photo: "",
+  });
+  const [file, setFile] = useState({
+    formal_photo: "",
+    signature_photo: "",
+  });
 
+  const onFormChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
 
-const handleFileChange = (event) => {
-    setFile(event.target.files);
-    console.log(file)
-}
-    const handleSubmit = (event) => {
+  const handleFileChange = (event) => {
+    setFile({ ...file, [event.target.name]: event.target.files });
+    console.log(file.formal_photo);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData();
-    for(var x = 0; x<file.length; x++) {
-        data.append('file', file[x])
-    }
-    axios.post("https://marine-form-backend.herokuapp.com/upload/file", data)
-    .then(res => { 
-        console.log(res);
-        setImgFile('https://marine-form-backend.herokuapp.com/public/images/'+ res.data.filename)
-      })
-}
+    let id = event.target.id;
+    // for (var x = 0; x < file.length; x++) {
+    data.append("file", file[`${id}`][0]);
+    // }
+    axios.post("https://marine-form-backend.herokuapp.com/upload/file", data).then((res) => {
+      console.log(res.data);
+      setImgFile({
+        ...imgFile,
+        [event.target.id]:
+          "https://marine-form-backend.herokuapp.com/public/images/" + res.data.filename,
+      });
+      setInputs({
+        ...inputs,
+        [event.target.id]:
+          "https://marine-form-backend.herokuapp.com/public/images/" + res.data.filename,
+      });
+    });
+    console.log(inputs.formal_photo);
+  };
 
-  const sendData = async () => {
-    handleSubmit();
+  const sendData = async (e) => {
+    e.preventDefault();
+
     const response = await fetch("https://marine-form-backend.herokuapp.com/upload", {
       method: "POST",
       headers: {
@@ -54,30 +74,44 @@ const handleFileChange = (event) => {
 
           <div class="row my-3" style={{ alignItems: "flex-end" }}>
             <div class="col-md-4">
-              <label>Date: </label> <input />
+              <label>Date: </label>{" "}
+              <input
+              type="date"
+                name="declaration_date"
+                value={inputs.declaration_date}
+                onChange={(e) => onFormChange(e)}
+              />
             </div>
 
             <div class="col-md-4">
-            <form >
-        <div className="form-group" >
+              <form>
+                <div className="form-group">
+                  <label htmlFor="file">Upload Formal Photo:</label>
+                  <input
+                    className="form-control-file mb-3"
+                    type="file"
+                    id="file"
+                    name="formal_photo"
+                    onChange={(e) => handleFileChange(e)}
+                  />
 
-            <label htmlFor="file">Upload Formal Photo:</label>
-            <input 
-            className="form-control-file mb-3" 
-            type="file" id="file" 
-            onChange={handleFileChange}
-            />
-
-            <button 
-            className="btn btn-primary mt-3" 
-            onClick={handleSubmit}
-            >Upload</button>
-        </div>
-       </form>
-       <div>
-
-            <img src={imgFile} width={100} height={100} alt="img"/>
-       </div>
+                  <button
+                    className="btn btn-primary mt-3"
+                    id="formal_photo"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    Upload
+                  </button>
+                </div>
+              </form>
+              <div>
+                <img
+                  src={imgFile.formal_photo}
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+              </div>
             </div>
 
             <div class="col-md-4">
@@ -87,9 +121,24 @@ const handleFileChange = (event) => {
               </label>{" "}
               <input
                 type="file"
-                encType="multipart/form-data"
-                name="signature"
+                name="signature_photo"
+                onChange={(e) => handleFileChange(e)}
               />
+              <button
+                className="btn btn-primary mt-3"
+                id="signature_photo"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Upload
+              </button>
+              <div>
+                <img
+                  src={imgFile.signature_photo}
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+              </div>
             </div>
           </div>
           <hr class="mt-3" />
@@ -100,8 +149,7 @@ const handleFileChange = (event) => {
               type="submit"
               title="Send"
               onClick={(e) => {
-                e.preventDefault();
-                sendData();
+                sendData(e);
               }}
             >
               Send
