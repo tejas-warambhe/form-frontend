54a9701397bf1ce9570caa903f988840f8a994df
Tejas <tejaswarambhe@gmail.com>
2022-07-18 01:19:39 +0530

    Commit Yesterday's Changes

--- src/components/SideBarAdmin.jsx
+++ src/components/SideBarAdmin.jsx
@@ -1,50 +1,65 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const SidebarAdmin = () => {
   return (
     <div>
-        <div>
-                    <form className="top-sticky"  >
-                        <div className="d-flex ms-3 flex-row-reverse">
-                            <div className="aside-tabs">
-                                <div className="nav flex-column nav-pills m-3 aside-tabs-scrolled" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                    <Link to={'/admin/edit/profile'}><button className="newbtn nav-link">Profile Details</button></Link>
-                                    <Link to={'/admin/edit/dependents'}><button className="newbtn nav-link" >Next of kin /
-                                        Dependents</button></Link>
-                                    <Link to={'/admin/edit/passport'}><button className="newbtn nav-link" >Passport details</button></Link>
-                                    <Link to={'/admin/edit/seamenbook'}><button className="newbtn nav-link">Continuous Discharge Certificate / Seamen
-                                        Book</button></Link>
-                                    <Link to={'/admin/edit/academicQualifications'}><button className="newbtn nav-link" >Academic
-                                        qualifications</button></Link>
-                                    <Link to={'/admin/edit/certificateofcompetency'}><button className="newbtn nav-link" >Licene / certificate of competency</button></Link>
-                                    <Link to={'/admin/edit/stcwandothercertificates'}><button className="newbtn nav-link" >STCW and other
-                                        certificate</button></Link>
-                                    <Link to={'/admin/edit/refrences'}><button className="newbtn nav-link">References</button></Link>
-                                    <Link to={'/admin/edit/howyouknowus'}><button className="newbtn nav-link" >How did you come to
-                                        know about us?</button></Link>
-                                    <Link to={'/admin/edit/personaldetails'}><button className="newbtn nav-link" >Personal details</button></Link>
-                                    <Link to={'/admin/edit/additionalinformation'}><button className="newbtn nav-link" >Additional
-                                        Information</button></Link>
-                                    <Link to={'/admin/edit/reasonofapplication'}><button className="newbtn nav-link">Reason for Application</button></Link>
-                                    <Link to={'/admin/edit/seaexperience'}><button className="newbtn nav-link">SEA
-                                        EXPERIENCE:<small>(Most recent at
-                                            Top)Special Experience:</small></button></Link>
-                                    <Link to={'/admin/edit/onshoreservice'}><button className="newbtn nav-link" >On-Shore
-                                        Service</button></Link>
-                                    <Link to={'/admin/edit/declaration'}><button className="newbtn nav-link">Declaration</button></Link>
-                                </div>
-                            </div>
-
-
-                            
-
-                        </div>
-                    </form>
-                </div>
+      <div>
+        <form className="top-sticky">
+          <div className="d-flex ms-3 flex-row-reverse">
+            <div className="aside-tabs">
+              <div
+                className="nav flex-column nav-pills m-3 aside-tabs-scrolled"
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation="vertical"
+              >
+                <NavLink to={"/admin/edit/profile"} className="newbtn nav-link">Profile Details</NavLink>
+                <NavLink to={"/admin/edit/dependents"} className="newbtn nav-link">
+                  Next of kin / Dependents
+                </NavLink>
+                <NavLink to={"/admin/edit/passport"} className="newbtn nav-link">Passport details</NavLink>
+                <NavLink to={"/admin/edit/seamenbook"} className="newbtn nav-link">
+                  Continuous Discharge Certificate / Seamen Book
+                </NavLink>
+                <NavLink to={"/admin/edit/academicQualifications"} className="newbtn nav-link">
+                  Academic qualifications
+                </NavLink>
+                <NavLink to={"/admin/edit/certificateofcompetency"} className="newbtn nav-link">
+                  Licene / certificate of competency
+                </NavLink>
+                <NavLink to={"/admin/edit/stcwandothercertificates"} className="newbtn nav-link">
+                  STCW and other certificate
+                </NavLink>
+                <NavLink to={"/admin/edit/refrences"} className="newbtn nav-link">References</NavLink>
+                <NavLink to={"/admin/edit/howyouknowus"} className="newbtn nav-link">
+                  How did you come to know about us?
+                </NavLink>
+                <NavLink to={"/admin/edit/personaldetails"} className="newbtn nav-link">
+                  Personal details
+                </NavLink>
+                <NavLink to={"/admin/edit/additionalinformation"} className="newbtn nav-link">
+                  Additional Information
+                </NavLink>
+                <NavLink to={"/admin/edit/reasonofapplication"} className="newbtn nav-link">
+                  Reason for Application
+                </NavLink>
+                <NavLink to={"/admin/edit/seaexperience"} className="newbtn nav-link">
+                  SEA EXPERIENCE:
+                  <small>(Most recent at Top)Special Experience:</small>
+                </NavLink>
+                <NavLink to={"/admin/edit/onshoreservice"} className="newbtn nav-link">
+                  On-Shore Service
+                </NavLink>
+                <NavLink to={"/admin/edit/declaration"} className="newbtn nav-link">Declaration</NavLink>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SidebarAdmin;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -10,30 +10,30 @@ const Sidebar = () => {
                         <div className="d-flex  flex-row-reverse">
                             <div className="aside-tabs">
                                 <div className="nav flex-column nav-pills m-3 aside-tabs-scrolled" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                    <Link to={'/'}><button className="newbtn nav-link">Profile Details</button></Link>
-                                    <Link to={'/dependents'}><button className="newbtn nav-link" >Next of kin /
-                                        Dependents</button></Link>
-                                    <Link to={'/passport'}><button className="newbtn nav-link" >Passport details</button></Link>
-                                    <Link to={'/seamenbook'}><button className="newbtn nav-link">Continuous Discharge Certificate / Seamen
-                                        Book</button></Link>
-                                    <Link to={'/academicQualifications'}><button className="newbtn nav-link" >Academic
-                                        qualifications</button></Link>
-                                    <Link to={'/certificateofcompetency'}><button className="newbtn nav-link" >Licene / certificate of competency</button></Link>
-                                    <Link to={'/stcwandothercertificates'}><button className="newbtn nav-link" >STCW and other
-                                        certificate</button></Link>
-                                    <Link to={'/refrences'}><button className="newbtn nav-link">References</button></Link>
-                                    <Link to={'/howyouknowus'}><button className="newbtn nav-link" >How did you come to
-                                        know about us?</button></Link>
-                                    <Link to={'/personaldetails'}><button className="newbtn nav-link" >Personal details</button></Link>
-                                    <Link to={'/additionalinformation'}><button className="newbtn nav-link" >Additional
-                                        Information</button></Link>
-                                    <Link to={'/reasonofapplication'}><button className="newbtn nav-link">Reason for Application</button></Link>
-                                    <Link to={'/seaexperience'}><button className="newbtn nav-link">SEA
+                                    <NavLink to={'/'} className="newbtn nav-link">Profile Details</NavLink>
+                                    <NavLink to={'/dependents'} className="newbtn nav-link">Next of kin /
+                                        Dependents</NavLink>
+                                    <NavLink to={'/passport'} className="newbtn nav-link">Passport details</NavLink>
+                                    <NavLink to={'/seamenbook'} className="newbtn nav-link">Continuous Discharge Certificate / Seamen
+                                        Book</NavLink>
+                                    <NavLink to={'/academicQualifications'} className="newbtn nav-link">Academic
+                                        qualifications</NavLink>
+                                    <NavLink to={'/certificateofcompetency'} className="newbtn nav-link">Licene / certificate of competency</NavLink>
+                                    <NavLink to={'/stcwandothercertificates'} className="newbtn nav-link">STCW and other
+                                        certificate</NavLink>
+                                    <NavLink to={'/refrences'} className="newbtn nav-link">References</NavLink>
+                                    <NavLink to={'/howyouknowus'} className="newbtn nav-link">How did you come to
+                                        know about us?</NavLink>
+                                    <NavLink to={'/personaldetails'} className="newbtn nav-link">Personal details</NavLink>
+                                    <NavLink to={'/additionalinformation'} className="newbtn nav-link">Additional
+                                        Information</NavLink>
+                                    <NavLink to={'/reasonofapplication'} className="newbtn nav-link">Reason for Application</NavLink>
+                                    <NavLink to={'/seaexperience'} className="newbtn nav-link">SEA
                                         EXPERIENCE:<small>(Most recent at
-                                            Top)Special Experience:</small></button></Link>
-                                    <Link to={'/onshoreservice'}><button className="newbtn nav-link" >On-Shore
-                                        Service</button></Link>
-                                    <Link to={'/declaration'}><button className="newbtn nav-link">Declaration</button></Link>
+                                            Top)Special Experience:</small></NavLink>
+                                    <NavLink to={'/onshoreservice'} className="newbtn nav-link">On-Shore
+                                        Service</NavLink>
+                                    <NavLink to={'/declaration'} className="newbtn nav-link">Declaration</NavLink>
                                 </div>
                             </div>
 
